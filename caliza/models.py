1266906e399794843b01db53cac521a92d10ed62
daniellobaton <daniellobaton@outlook.com>
2022-05-01 17:32:01 -0500

    Se implementa la funcionalidad básica del carrito y la página de pago

--- caliza/models.py
+++ caliza/models.py
@@ -18,6 +18,14 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+        
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
 
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete = models.SET_NULL, blank = True, null = True)
@@ -27,12 +35,29 @@ class Order(models.Model):
 
     def __str__(self):
         return str(self.id)
+    
+    @property
+    def getCartTotal(self):
+        orderItems = self.orderitem_set.all()
+        total = sum([item.getTotal for item in orderItems])
+        return total
+
+    @property
+    def getCartItems(self):
+        orderItems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderItems])
+        return total
 
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete = models.SET_NULL, blank = True, null = True)
     order = models.ForeignKey(Order, on_delete = models.SET_NULL, blank = True, null = True)
     quantity = models.IntegerField(default = 0, null = True, blank = True)
     dateAdded = models.DateTimeField(auto_now_add = True)
+
+    @property 
+    def getTotal(self):
+        total = self.product.price * self.quantity
+        return total
 
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer, on_delete = models.SET_NULL, null = True)
--- caliza/views.py
+++ caliza/views.py
@@ -8,9 +8,25 @@ def store(request):
     return render(request, 'caliza/store.html', context)
 
 def cart(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer = customer, complete = False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'getCartTotal': 0, 'getCartItems': 0}
+
+    context = {'items': items, 'order': order}
     return render(request, 'caliza/cart.html', context)
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer = customer, complete = False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'getCartTotal': 0, 'getCartItems': 0}
+
+    context = {'items': items, 'order': order}
     return render(request, 'caliza/checkout.html', context)
