d1510f7f2a87087e4fc489b982d197437f5b7709
daniellobaton <daniellobaton@outlook.com>
2022-05-01 15:54:49 -0500

    Se enlaza la carpeta static/img con la base de datos para que se pueden empezar a cargar imágenes en el sitio web

--- caliza/views.py
+++ caliza/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def store(request):
-    context = {}
+    products = Product.objects.all()
+    context = {'products': products}
     return render(request, 'caliza/store.html', context)
 
 def cart(request):
