022278972f66585afbc43af5d24f14a8669b13cd
daniellobaton <daniellobaton@outlook.com>
2022-05-01 21:46:51 -0500

    Se establece la lógica del carrito de compras

--- caliza/views.py
+++ caliza/views.py
@@ -1,10 +1,23 @@
+from cmath import log
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import *
+import json
 
 # Create your views here.
 def store(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer = customer, complete = False)
+        items = order.orderitem_set.all()
+        cartItems = order.getCartItems
+    else:
+        items = []
+        order = {'getCartTotal': 0, 'getCartItems': 0}
+        cartItems = order['getCartItems']
+
     products = Product.objects.all()
-    context = {'products': products}
+    context = {'products': products, 'cartItems': cartItems}
     return render(request, 'caliza/store.html', context)
 
 def cart(request):
@@ -12,11 +25,14 @@ def cart(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer = customer, complete = False)
         items = order.orderitem_set.all()
+        cartItems = order.getCartItems
     else:
         items = []
         order = {'getCartTotal': 0, 'getCartItems': 0}
+        cartItems = order['getCartItems']
 
-    context = {'items': items, 'order': order}
+
+    context = {'items': items, 'order': order, 'cartItems': cartItems}
     return render(request, 'caliza/cart.html', context)
 
 def checkout(request):
@@ -24,9 +40,38 @@ def checkout(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer = customer, complete = False)
         items = order.orderitem_set.all()
+        cartItems = order.getCartItems
+
     else:
         items = []
-        order = {'getCartTotal': 0, 'getCartItems': 0}
+        order = {'getCartTotal': 0, 'getCartItems': 0, 'cartItems': cartItems}
+        cartItems = order['getCartItems']
 
     context = {'items': items, 'order': order}
     return render(request, 'caliza/checkout.html', context)
+
+def updateItem(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+
+    print('Product id: ', productId)
+    print('Action: ', action)
+
+    customer = request.user.customer
+    product = Product.objects.get(id = productId)
+    order, created = Order.objects.get_or_create(customer = customer, complete = False)
+
+    orderItem, created = OrderItem.objects.get_or_create(order = order, product = product)
+
+    if action == 'add':
+        orderItem.quantity += 1
+    elif action == 'remove':
+        orderItem.quantity -= 1
+
+    orderItem.save()
+
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+
+    return JsonResponse('Item was added', safe=False)
---
+++ static/js/cart.js
@@ -0,0 +1,49 @@
+var updateBtns = document.getElementsByClassName('update-cart');
+
+for(let i = 0; i < updateBtns.length; i++){
+
+    updateBtns[i].addEventListener('click', function(){
+
+        let productId = this.dataset.product;
+        let action = this.dataset.action;
+
+        console.log('productId: ', productId, 'action: ', action);
+
+        console.log('User: ', user);
+        if(user === 'AnonymousUser'){
+
+            console.log('Not logged in');
+
+        }else{
+
+            updateUserOrder(productId, action);
+
+        }
+
+    })
+
+}
+
+function updateUserOrder(productId, action){
+
+    console.log('User is logged in');
+
+    var url = '/update_item/';
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken,
+        },
+        body: JSON.stringify({'productId': productId, 'action': action})
+    })
+
+    .then((response) =>{
+        return response.json();
+    })
+    .then((data) =>{
+        console.log('data: ', data);
+        location.reload();
+    })
+}
