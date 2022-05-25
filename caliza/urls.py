1fbd19fd547cacb751185f3f2d940815d96dd8c4
grtelis <elizabeth_torres@comunidad.unam.mx>
2022-05-25 17:41:49 -0500

    CAL-59 pagina individual de cada producto

--- caliza/urls.py
+++ caliza/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     
     path('update_item/', views.updateItem, name='update_item'),
     path('process_order/', views.processOrder, name='process_order'),
-    path('individual_product/', views.individual, name='individual_product'),
+    path('individualProduct/', views.individualProduct, name='individual_product'),
     path('promos/', views.promos, name='promos'),
     path('masVendidos/', views.masVendidos, name='masVendidos'),
 ]
