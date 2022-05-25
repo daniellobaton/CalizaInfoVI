ee3393f8c6eed3a82ddbe5c4322c46f459f7a2e4
daniellobaton <daniellobaton@outlook.com>
2022-05-25 18:42:38 -0500

    Se modifica el modelo producto y se establece el método get en store.html

--- caliza/models.py
+++ caliza/models.py
@@ -15,6 +15,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits = 7, decimal_places = 2)
     digital = models.BooleanField(default = False, null = True, blank = False)
     image = models.ImageField(null = True, blank = True)
+    totalSell = models.IntegerField()
 
     def __str__(self):
         return self.name
