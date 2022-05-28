156cb0044b7b4f0dff09d62fb5281a1ef5c02fd8
daniellobaton <daniellobaton@outlook.com>
2022-05-28 00:18:45 -0500

    Se agrega la funcionalidad básica de despliegue de producto individual y generación de la transacción de venta del mismo

--- caliza/views.py
+++ caliza/views.py
@@ -1,9 +1,10 @@
+import re
 from django.shortcuts import render
 from django.http import JsonResponse
 import json
 import datetime
 from .models import *
-from . utils import cookieCart, cartData, guestOrder
+from . utils import cookieCart, cartData, guestOrder, individualPurchase
 
 # Import pagination stuff
 from django.core.paginator import Paginator
@@ -23,16 +24,40 @@ def cart(request):
     order = data['order']
     items = data['items']
 
+    #print('La request es: ', request)
+
     context = {'items': items, 'order': order, 'cartItems': cartItems}
     return render(request, 'caliza/cart.html', context)
 
 def checkout(request):
-    data = cartData(request)
-    cartItems = data['cartItems']
-    order = data['order']
-    items = data['items']
 
-    context = {'items': items, 'order': order, 'cartItems': cartItems}
+    if request.GET:
+        productId = request.GET['producto']
+        cantidad = request.GET['cantidad']
+        #print('Todo correcto!')
+        #print(productId)
+        #print(cantidad)
+        iterable = False
+
+        producto = Product.objects.get(id = productId)
+        #print(producto.price)
+        #print(cantidad)
+        #total = 0
+        total = int(producto.price) * int(cantidad)
+        datos = individualPurchase(request)
+        order = datos['order'] 
+
+        context = {'items': producto, 'quantity': cantidad, 'iterable': iterable, 'total': total, 'order': order}
+    else:
+
+        data = cartData(request)
+        cartItems = data['cartItems']
+        order = data['order']
+        items = data['items'] #arreglo con los productos del carrito
+        iterable = True
+
+        context = {'items': items, 'order': order, 'cartItems': cartItems, 'iterable': iterable}
+
     return render(request, 'caliza/checkout.html', context)
 
 def ourProducts(request):
@@ -50,12 +75,16 @@ def ourProducts(request):
     return render(request, 'caliza/ourProducts.html', context)
     
 def individualProduct(request):
+    #print(request.GET)
+    productId = request.GET['producto']
+    product = Product.objects.get(id = productId)
+    #print('Precio del producto: ', product.price)
     data = cartData(request)
     cartItems = data['cartItems']
     order = data['order']
     items = data['items']
 
-    context = {'items': items, 'order': order, 'cartItems': cartItems}
+    context = {'items': items, 'order': order, 'cartItems': cartItems, 'producto': product}
     return render(request, 'caliza/individualProduct.html', context)    
 
 def promos(request):
@@ -105,8 +134,15 @@ def updateItem(request):
 #from django.views.decorators.csrf import csrf_exempt
 #@csrf_exempt
 def processOrder(request):
+    data = cookieCart(request)
+    items = data['items']
+
+    #print('productos: ', items[0]['product']['id'])
+
     transactionId = datetime.datetime.now().timestamp()
     data = json.loads(request.body)
+
+    print("Datos: ", data)
 
     if request.user.is_authenticated:
         customer = request.user.customer
@@ -120,6 +156,7 @@ def processOrder(request):
 
     if total == order.getCartTotal:
         order.complete = True
+
     order.save()
 
     if order.shipping == True:
